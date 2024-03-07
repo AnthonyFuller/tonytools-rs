36bca3a10e48852d52c72b7ec438d0e2dece6fd8
Anthony Fuller <anthony@hitmods.com>
2024-03-07 19:47:43 +0000

    feat(textures, bytereader): initial work (#1)
    * FEATURE: util::ByteReader and hmtextures::hm2016::Texture::load
    
    * feat(hmtextures): working test
    
    * fix(hm2016): magic reading
    
    * Feat: hmtextures::hm2016::Texture::load and ByteReader
    
    ---------
    
    Co-authored-by: KingPEPSALT <68469008+KingPEPSALT@users.noreply.github.com>

---
+++ src/hmtextures/hm2016.rs
@@ -0,0 +1,108 @@
+use std::{
+    array::TryFromSliceError, io::BufRead,
+};
+
+use crate::{
+    hmtextures::{self, Format, Type},
+    util::bytereader::{ByteReader, ByteReaderError},
+};
+#[derive(Default, Debug)]
+struct Texture {
+    pub magic: u16,
+    pub metadata: MetaData,
+    pub file_size: u32,
+    pub width: u16,
+    pub height: u16,
+    pub mips_count: u8,
+    pub default_mip: u8,
+    pub dimensions: u8,
+    pub atlas_size: u32,
+    pub atlas_offset: u32,
+
+    pub mips_datasizes: [u32; 0xE],
+
+    pub pixels: Vec<u8>,
+}
+
+#[derive(Default, Debug)]
+struct MetaData {
+    pub r#type: Type,
+    pub format: Format,
+
+    pub flags: u32,
+
+    pub interpret_as: u8,
+    pub mips_interpol_mode: u8,
+}
+
+impl Texture {
+    pub fn load(data: &[u8]) -> Result<Self, hmtextures::Error> {
+        let mut buf = ByteReader::new(data);
+        let mut texture = Texture::default();
+
+        texture.magic = match buf.read::<u16,  2>() {
+            Ok(1) => Ok(1),
+            Ok(_) => Err(hmtextures::Error::InvalidMagic),
+            Err(e) => Err(e.into()),
+        }?;
+
+        texture.metadata.r#type = match buf.read::<u16, 2>() {
+            Ok(n @ 0..=3) => n.try_into().map_err(|_| hmtextures::Error::UnknownType),
+            Ok(_) => Err(hmtextures::Error::UnknownType),
+            Err(e) => Err(e.into()),
+        }?;
+
+        buf.consume(4); // SKIP TEXD
+
+        if let [fs, fl] = buf.read_n::<u32, 4>(2)?[..] {
+            [texture.file_size, texture.metadata.flags] = [fs, fl];
+        };
+        if let [w, h] = buf.read_n::<u16, 2>(2)?[..] {
+            [texture.width, texture.height] = [w, h];
+        };
+
+        if let Ok(fmt) = buf.read::<u16, 2>()?.try_into() {
+            texture.metadata.format = fmt;
+        };
+        if let [mc, dm, ia, dim, mim] = buf.read_n::<u8, 1>(5)?[..] {
+            [
+                texture.mips_count,
+                texture.default_mip,
+                texture.metadata.interpret_as,
+                texture.dimensions,
+                texture.metadata.mips_interpol_mode,
+            ] = [mc, dm, ia, dim, mim];
+        }
+        buf.consume(1);
+        if texture.dimensions != 0 {
+            return Err(hmtextures::Error::InvalidDimensions);
+        }
+
+        if let Ok(mds) =
+            buf.read_n::<u32, 4>(14)?.as_slice().try_into() as Result<[u32; 14], TryFromSliceError>
+        {
+            texture.mips_datasizes = mds;
+        } else {
+            return Err(hmtextures::Error::ByteReaderError(ByteReaderError::NoBytes));
+        };
+
+        if let [a_s, a_o] = buf.read_n::<u32, 4>(2)?[..] {
+            [texture.atlas_size, texture.atlas_offset] = [a_s, a_o];
+        }
+        texture.pixels = buf.fill_buf()?.to_vec();
+        Ok(texture)
+    }
+}
+
+pub fn convert(data: &[u8], output_path: &str, metadata_path: &str, swizzle: bool) {
+    unimplemented!();
+}
+
+#[test]
+fn test_2016() -> Result<(), hmtextures::Error> {
+    let file = std::fs::read("texture.text")?;
+    let mut texture = Texture::load(file.as_slice());
+
+    println!("{:#?}", texture);
+    Ok(())
+}
---
+++ src/hmtextures/mod.rs
@@ -0,0 +1,108 @@
+use std::{convert::Infallible, default, io, num::TryFromIntError};
+
+use crate::util::bytereader::ByteReaderError;
+
+pub mod hm2016;
+
+#[derive(Debug)]
+enum Error {
+    InvalidMagic,
+    InvalidDimensions,
+    UnknownType,
+    UnknownFormat,
+    ByteReaderError(ByteReaderError),
+    IOError(io::Error),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::IOError(err)
+    }
+}
+
+impl From<ByteReaderError> for Error {
+    fn from(err: ByteReaderError) -> Self {
+        Error::ByteReaderError(err)
+    }
+}
+
+#[derive(Default, Debug, Clone, Copy)]
+enum Type {
+    Colour,
+    Normal,
+    Height,
+    CompoundNormal,
+    Billboard,
+    #[default]
+    Unknown,
+}
+impl From<Type> for u16 {
+    fn from(r#type: Type) -> Self {
+        r#type as u16
+    }
+}
+impl TryFrom<u16> for Type {
+    type Error = self::Error;
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        Ok([
+            Self::Colour,
+            Self::Normal,
+            Self::Height,
+            Self::CompoundNormal,
+            Self::Billboard,
+            Self::Unknown
+            ][(value.try_into() as Result<usize, Infallible>).map_err(|_| self::Error::UnknownType)?]
+        )
+    }
+}
+#[derive(Default, Debug)]
+enum Format {
+    #[default]
+    Unknown = 0,
+    R16G16B16A16 = 0x0A,
+    R8G8B8A8 = 0x1C,
+    R8G8 = 0x34, //Normals. very rarely used. Legacy? Only one such tex in chunk0;
+    A8 = 0x42,   //8-bit grayscale uncompressed. Not used on models?? Overlays
+    DXT1 = 0x49, //Color maps, 1-bit alpha (mask). Many uses, color, normal, spec, rough maps on models and decals. Also used as masks.
+    DXT3 = 0x4C,
+    DXT5 = 0x4F, //Packed color, full alpha. Similar use as DXT5.
+    BC4 = 0x52,  //8-bit grayscale. Few or no direct uses on models?
+    BC5 = 0x55,  //2-channel normal maps
+    BC7 = 0x5A,  //high res color + full alpha. Used for pretty much everything...
+
+}
+impl From<Format> for u16 {
+    fn from(format: Format) -> Self {
+        format as u16
+    }
+}
+impl TryFrom<u16> for Format {
+    type Error = self::Error;
+    fn try_from(value: u16) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Self::Unknown),
+            0x0A => Ok(Self::R16G16B16A16),
+            0x1C => Ok(Self::R8G8B8A8),
+            0x34 => Ok(Self::R8G8),
+            0x42 => Ok(Self::A8),
+            0x49 => Ok(Self::DXT1),
+            0x4C => Ok(Self::DXT3),
+            0x4F => Ok(Self::DXT5),
+            0x52 => Ok(Self::BC4),
+            0x55 => Ok(Self::BC5),
+            0x5A => Ok(Self::BC7),
+            _ => Err(self::Error::UnknownFormat)
+        }
+    }
+}
+struct BuiltTexture<'a> {
+    pub width: u16,
+    pub height: u16,
+    pub mips_count: u8,
+
+    pub mips_sizes: [u32; 0xE],
+    pub compressed_sizes: [u32; 0xE],
+
+    pub pixels: &'a [u8],
+    pub compressed_pixels: &'a [u8],
+}
