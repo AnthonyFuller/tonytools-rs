f9ea56a79e2e725edc17b3578e98dac2257373b5
AnthonyFuller <anthony@hitmods.com>
2024-03-16 02:51:17 +0000

    chore: add copy and clone to Version enum

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ pub mod hmlanguages;
 pub mod hmtextures;
 pub mod util;
 
-#[derive(Default, Debug, PartialEq)]
+#[derive(Default, Debug, PartialEq, Copy, Clone)]
 pub enum Version {
     H2016,
     H2,
